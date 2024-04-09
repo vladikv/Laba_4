b9b532d256756c377cf2cea9a226a8a55f79b5e3
vladik <121016003+vladikv@users.noreply.github.com>
2024-04-10 00:36:43 +0300

    Class Car and Curency

---
+++ Curency/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Currency
+{
+    public string Name { get; set; }
+    public double ExchangeRate { get; set; }
+}
+
+class CurrencyExchange
+{
+    private Dictionary<string, int> availableCurrencies; 
+    private List<Currency> currencies; 
+
+    public CurrencyExchange()
+    {
+        currencies = new List<Currency>();
+        availableCurrencies = new Dictionary<string, int>();
+    }
+
+    public void AddCurrency(Currency currency, int amount)
+    {
+        currencies.Add(currency);
+        availableCurrencies[currency.Name] = amount;
+    }
+
+    public void ExchangeCurrency(string fromCurrency, string toCurrency, double amount)
+    {
+        var from = currencies.FirstOrDefault(c => c.Name == fromCurrency);
+        var to = currencies.FirstOrDefault(c => c.Name == toCurrency);
+
+        if (from == null || to == null)
+        {
+            Console.WriteLine("Invalid currency.");
+            return;
+        }
+
+        double exchangedAmount = amount * from.ExchangeRate / to.ExchangeRate;
+        availableCurrencies[fromCurrency] -= (int)amount;
+        availableCurrencies[toCurrency] += (int)exchangedAmount;
+
+        Console.WriteLine($"Exchanged {amount} {fromCurrency} to {exchangedAmount} {toCurrency}");
+    }
+
+    public void PrintAvailableCurrencies()
+    {
+        Console.WriteLine("Available currencies in exchange:");
+        foreach (var currency in currencies)
+        {
+            Console.WriteLine($"{currency.Name} - {availableCurrencies[currency.Name]}");
+        }
+    }
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CurrencyExchange exchange = new CurrencyExchange();
+
+        exchange.AddCurrency(new Currency { Name = "USD", ExchangeRate = 1 }, 1000);
+        exchange.AddCurrency(new Currency { Name = "EUR", ExchangeRate = 0.85 }, 800);
+        exchange.AddCurrency(new Currency { Name = "GBP", ExchangeRate = 0.72 }, 700);
+        exchange.AddCurrency(new Currency { Name = "JPY", ExchangeRate = 110.23 }, 500);
+        exchange.AddCurrency(new Currency { Name = "CAD", ExchangeRate = 1.25 }, 600);
+        exchange.AddCurrency(new Currency { Name = "AUD", ExchangeRate = 1.30 }, 550);
+
+        exchange.ExchangeCurrency("USD", "EUR", 200);
+
+        exchange.PrintAvailableCurrencies();
+    }
+}
---
+++ Laba_4/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+
+class Car
+{
+    public string brand;
+    public string color;
+
+    public Car(string brand, string color)
+    {
+        this.brand = brand;
+        this.color = color;
+    }
+
+
+    public virtual void ChangeBrand(string newBrand)
+    {
+        brand = newBrand;
+        Console.WriteLine("Car brand changed.");
+    }
+
+
+    public virtual void ChangeColor(string newColor)
+    {
+        color = newColor;
+        Console.WriteLine("Car color changed.");
+    }
+
+
+    public override string ToString()
+    {
+        return $"Brand: {brand}, Color: {color}";
+    }
+
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        var car = (Car)obj;
+        return brand == car.brand && color == car.color;
+    }
+
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(brand, color);
+    }
+}
+
+
+class Truck : Car
+{
+    private double loadCapacity;
+
+    public Truck(string brand, string color, double loadCapacity) : base(brand, color)
+    {
+        this.loadCapacity = loadCapacity;
+    }
+
+
+    public void ChangeLoadCapacity(double newLoadCapacity)
+    {
+        loadCapacity = newLoadCapacity;
+        Console.WriteLine("Truck load capacity changed.");
+    }
+}
+
+
+class PassengerCar : Car
+{
+    public PassengerCar(string brand, string color) : base(brand, color)
+    {
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car1 = new Truck("Volvo", "black", 10.5);
+        Car car2 = new PassengerCar("Toyota", "red");
+
+        car1.ChangeBrand("Tesla");
+        car1.ChangeColor("white");
+
+        car2.ChangeBrand("BMW");
+        car2.ChangeColor("blue");
+
+        Console.WriteLine(car1.ToString());
+        Console.WriteLine(car2.ToString());
+    }
+}
